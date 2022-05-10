a4ea7e5097ac9e15b7af3d716a1168dbd4b261db
Connor <zittrauerconnor@gmail.com>
2022-05-09 22:05:42 -0500

    learning functions

---
+++ functions/src/main.rs
@@ -0,0 +1,14 @@
+// fn main() {
+//     function_one(14, -72);
+// }
+
+// //in function signatures, you MUST declare the type of each parameter
+// fn function_one(y: i32, z: i8) {
+//     println!("The value of y is {}", y);
+//     println!("The value of z is {}", z);
+// }
+
+fn main() {
+    // let y = 123;
+    let x = (let y = 123);
+}
