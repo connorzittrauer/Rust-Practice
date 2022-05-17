9df0d6658b37f7dccd3762d3ffb3c107233fdfec
connor <zittrauerconnor@gmail.com>
2022-05-17 17:08:59 -0500

    better calculator

--- farenheit_to_celcius_calculator/src/main.rs
+++ farenheit_to_celcius_calculator/src/main.rs
@@ -1,35 +1,40 @@
-use std::io;
-use std::io::Write;
+// //reddit improvement
+use std::io::{self, Write as _};
+
 
 fn main() {
+    //mutable  variable, set by the user
+    let mut fahrenheit = String::new();
 
-        loop {
-            //get farenheit input from user
-            print!("Input the degrees in farenheit (numbers only): ");
-            io::stdout().flush().unwrap();
-    
+    loop {
+        fahrenheit.clear();
 
-            //mutable farenheit variable set by user
-            let mut farenheit = String::new();
-        
-            io::stdin().read_line(&mut farenheit)
-                .expect("Failed to read input");    
+        //get input from user
+        print!("Input the degrees in fahrenheit: ");
+        io::stdout().flush().unwrap();
 
-            let farenheit: i32 = match farenheit.trim().parse(){
-                Ok(num) => num,
-                Err(_) => continue,
+        io::stdin()
+            .read_line(&mut fahrenheit)
+            .expect("Erroneous input");
 
-            };
+        let fahrenheit = fahrenheit.trim();
+        if fahrenheit.eq_ignore_ascii_case("q") {
+            break;
+        }    
 
-          
+        let fahrenheit: f64 = match fahrenheit.parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-            let farenheit_float = farenheit as f64;
-         
-            let celcius: f64 = (farenheit_float - 32.00) * 0.5556;
-            
-            println!("{} degrees farenheit is {} degrees celcius \n", farenheit, celcius);
-            
-        }
+        let celsius = (fahrenheit - 32.00) * 0.5556;
 
+        println! (
+            "{} degrees fahrenheit is {} degrees celsius \n",
+            fahrenheit, celsius
+        );
+
+
+    }
 
 }
