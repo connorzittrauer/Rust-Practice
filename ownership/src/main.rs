016acfd7309ed5e064c2509852720ddc0aefa227
connor zittrauer <zittrauerconnor@gmail.com>
2022-05-18 12:02:29 -0500

    immutable vs mutable reference

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    // let mut name = String::from("bob");
+    // {
+    //     let reference1 = &mut name;
+    // } //multiple reference allowed because reference1 goes out of scope at this project, this is compilable
+
+    // let reference2 = &mut name;
+
+
+
+
+
+}
+
+//mutable vs immutable references
+fn main {
+    let mut name = String::from("bob");
+    let r1 = &mut name; //the push_str works because of the reference to &mut
+    //let r1 = &name; //this fails because the the &name cannot be borrowed as mutable
+
+    r1.push_str(" smith");
+    
+    
+    println!("{}", name);
+
 }
